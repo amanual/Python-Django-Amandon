7b4f798788134e517262e2a2eae6f9ff3265fa7b
Amanual <amanual2@gmail.com>
2017-09-22 13:36:09 -0400

    added djangon_amadon_assignment

---
+++ main/apps/amadon/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, redirect
+from time import gmtime, strftime
+
+# Create your views here.
+def index(request):
+    # print request.POST['product_id']
+    
+
+    return render(request, 'amadon/index.html')
+    
+def amadon(request):
+    
+    print int(request.POST['quantity'])
+    result = int(request.POST['quantity'])    
+       
+    if 'count' in request.session: 
+        # request.session['count'] += request.session['count'] 
+        # request.session['total'] += request.session['total']
+        print request.session['total'] 
+        print request.session['count']   
+
+        if 'quantity' in request.POST and request.POST['product_id'] == '1015':            
+            request.session['iphone']= result * 700            
+            request.session['count'] += result
+            request.session['total'] += request.session['iphone']
+
+        if 'quantity' in request.POST and request.POST['product_id'] == '1016':                
+            request.session['iphone']= result * 800
+            request.session['count'] += result
+            request.session['total'] += request.session['iphone']
+            
+        if 'quantity' in request.POST and request.POST['product_id'] == '1017':                
+            request.session['iphone']= result * 900
+            request.session['count'] += result
+            request.session['total'] += request.session['iphone']
+
+        if 'quantity' in request.POST and request.POST['product_id'] == '1018':                
+            request.session['iphone']= result * 1000
+            request.session['count'] += result
+            request.session['total'] += request.session['iphone']
+
+       
+        
+
+
+
+
+        # request.session['shop'].append(data)
+        # print request.session['shop']
+    #     # request.POST['product_id'] == '1015':
+    #     print request.POST['quantity']
+        # print request.POST['porduct_id']
+        # iphone= int(request.POST['quantity']) * 900
+        # request.POST['product_id']
+        # request.session['shop'].append(data)
+    # else:
+    #     print 'you did not order'
+        
+        
+    return render(request, 'amadon/display.html')
+# def reset(request):
+#     request.session.clear()
+#     return redirect('/')
